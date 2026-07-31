3feeb00b1214721b6ceb8229a6aaf910f3a55af3
Nishant Chauhan <chauhanjaat942@gmail.com>
2026-07-31 11:11:33 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js'
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
+
+
+app.listen(PORT, ()=> console.log(`Server running at http://localhost:${PORT}`));
