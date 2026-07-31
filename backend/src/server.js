2c952091944d5e8c33f69cac6f061a3a0da0310a
Nishant Chauhan <chauhanjaat942@gmail.com>
2026-07-31 13:15:00 +0530

    Fix frontend static path for Render

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,11 +18,12 @@ app.use('/api/message', messageRoutes);
 
 
 // make ready for deployment
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
-    app.get("*", (_, res)=>{
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    })
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    });
 }
 
 app.listen(PORT, ()=> console.log(`Server running at http://localhost:${PORT}`));
