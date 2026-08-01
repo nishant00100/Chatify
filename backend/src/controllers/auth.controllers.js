c971b57f38cff9886d2bbf14c5bc5a7ceaac92b4
Nishant Chauhan <chauhanjaat942@gmail.com>
2026-08-01 16:58:53 +0530

    login & logout functionality added

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -76,10 +76,38 @@ export const register = async (req, res) => {
 
 // user login
 export const login = async (req, res) => {
-    return res.send("Login endPoint");
+    const {email, password} = req.body;
+
+    try{
+        const user = await User.findOne({email});
+        if(!user) return res.status(400).json({
+            message: "Invalid email or password"
+        })
+
+        const isPawwsordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPawwsordCorrect) return res.status(400).json({
+            message: "Invalid email or password"
+        })
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+
+    } catch(error){
+        console.error("Error in login controller: ", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
 }
 
 // user logout
-export const logout = async (req, res) => {
-    return res.send("Logout endPoint");
+export const logout = async (_, res) => {
+    res.cookie("jwt", "", {
+        maxAge: 0,
+    });
+    res.status(200).json({ message: "Logged out successfully" });
 }
