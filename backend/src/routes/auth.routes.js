93cceb28b99e3cd915cbd866ca9c0b381ade66d7
Nishant Chauhan <chauhanjaat942@gmail.com>
2026-08-01 18:16:35 +0530

    update profile endpoint added

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,10 +1,18 @@
 import express from 'express';
-import { register, login, logout } from '../controllers/auth.controllers.js';
+import { register, login, logout, updateProfile } from '../controllers/auth.controllers.js';
+import { protectRoute } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', logout);    
+router.post('/logout', logout);  
+
+
+router.post('/update-profile', protectRoute, updateProfile);    
+
+router.get('/check', protectRoute, (req, res) => {
+    res.status(200).json({ message: "User is authenticated", user: req.user });
+});
 
 export default router;
