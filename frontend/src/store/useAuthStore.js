8ba47268fbcfb12a00e10706f34ac1fe7933a4c3
Nishant Chauhan <chauhanjaat942@gmail.com>
2026-08-02 15:49:09 +0530

    login and logout functionality added

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -7,6 +7,7 @@ export const useAuthStore = create((set) => ({
     authUser: null,
     isCheckingAuth: true,
     isSigningUp: false,
+    isLoggingIn: false,
 
     checkAuth: async () => {
         try {
@@ -40,6 +41,40 @@ export const useAuthStore = create((set) => ({
         }
         finally {
             set({ isSigningUp: false });
+        }
+    },
+
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+
+            const response = await axiosInstance.post("/auth/login", data);
+            set({ authUser: response.data });
+
+            // toast
+            toast.success("Logged in successfully");
+
+
+        } catch (error) {
+            console.log("Full error:", error);
+            console.log("Response:", error.response);
+            console.log("Data:", error.response?.data);
+
+            toast.error(error.response?.data?.message || "Something went wrong");
+        }
+        finally {
+            set({ isLoggingIn: false });
+        }
+    },
+
+    logout: async()=>{
+        try{
+            await axiosInstance.post("/auth/logout");
+            set({ authUser: null });
+            toast.success("Logged out successfully");
+        } catch(error) {
+            toast.error("Error logging out");
+            console.log("Logout Error: ", error);
         }
     }
 }))
